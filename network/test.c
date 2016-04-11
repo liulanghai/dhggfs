a550af528088d600ca256294f152ac7ee8d60c43
dhg <416854993@qq.com>
2016-04-11 17:33:28 +0800

    epoll  test success

--- network/test.c
+++ network/test.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define MAXLINE 5
+#define MAXLINE 10000
 #define OPEN_MAX 100
 #define LISTENQ 20
 #define SERV_PORT 5000
@@ -14,8 +14,8 @@
 int main(int argc, char *argv[])
 {
  	struct sockaddr_in clientaddr;
-	int sockfd;
-	int i=0,n,connfd ,epfd,nfds; 
+	int sockfd,len=0;
+	int i=0,n,connfd =0 ,epfd,nfds; 
 	char line[MAXLINE];
 	struct epoll_event ev,events[20];
    	int listenfd=create_socket_and_listen(INADDR_ANY,2456,5);
@@ -24,79 +24,71 @@ int main(int argc, char *argv[])
 		printf("listen erro %d\n",listenfd);
 		return 1 ;
 	}	
+	len=sizeof(clientaddr);
 	char buf[100]={0};
+	epfd=epoll_create(256);
+	ev.data.fd=listenfd;
+        ev.events=EPOLLIN|EPOLLET;
+        epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&ev);
+
 	for ( ; ; ) 
 	{
-        //等待epoll事件的发
      	nfds=epoll_wait(epfd,events,20,500);
-		ev.data.fd=listenfd;
-		ev.events=EPOLLIN|EPOLLET;
-		epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&ev);
-       	 //处理所发生的所有事件
-		epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&ev);
-        for(i=0;i<nfds;++i)
+        for(i=0;i<nfds;i++)
         {
             if(events[i].data.fd==listenfd)//如果新监测到一个SOCKET用户连接到了绑定的SOCKET端口，建立新的连接。
 
             {
-                connfd = accept(listenfd,(struct sockaddr *)&clientaddr, sizeof(clientaddr));
+                connfd = accept(listenfd,(struct sockaddr *)&clientaddr, &len);
                 if(connfd<0)
 				{
                     perror("connfd<0");
                     exit(1);
                 }
-                //setnonblocking(connfd);
-
-               // char *str = inet_ntoa(clientaddr.sin_addr);
-              //  cout << "accapt a connection from " << str << endl;
-                //设置用于读操作的文件描述符
-
+				printf("a new client connect \n");
                 ev.data.fd=connfd;
-                //设置用于注测的读操作事件
-
                 ev.events=EPOLLIN|EPOLLET;
-                //ev.events=EPOLLIN;
-
-                //注册ev
-
                 epoll_ctl(epfd,EPOLL_CTL_ADD,connfd,&ev);
             }
-            else if(events[i].events&EPOLLIN)//如果是已经连接的用户，并且收到数据，那么进行读入。
-
-            {
-               // cout << "EPOLLIN" << endl;
-                if ( (sockfd = events[i].data.fd) < 0)
-                    continue;
-                if ( (n = read(sockfd, line, MAXLINE)) < 0) 
-					{
-	                    if (errno == ECONNRESET) 
+ 	//          if(events[i].events&EPOLLIN)//如果是已经连接的用户，并且收到数据，那么进行读入。
+      else       {
+			//	if ( (sockfd = events[i].data.fd) < 0)
+             //   {
+			//		  printf("sockfd = %d\n",sockfd);
+			//		  continue;
+			//	}
+                if ( (n = read(events[i].data.fd, line, MAXLINE)) < 0) 
+				{
+					 if (errno == ECONNRESET) 
 						{
+							printf("rest\n");
 	                        close(sockfd);
 	                        events[i].data.fd = -1;
-                   		 } else
-                       // std::cout<<"readline error"<<std::endl;
-							printf("read erro");
+                   		 }
+					 else
+						printf("read erro");
 			
-               		 } else if (n == 0) {
+               	 }
+				 else if (n == 0)
+				 {
+					printf("read no data\n");
+					break;
                     close(sockfd);
                     events[i].data.fd = -1;
-                }
-                line[n] = '/0';
-					printf("read ：%s",line);
-                //cout << "read " << line << endl;
-                //设置用于写操作的文件描述符
+                 }
+                line[n] = '\0';
+				printf("read ：%s\n",line);
 
                 ev.data.fd=sockfd;
                 //设置用于注测的写操作事件
 
-                ev.events=EPOLLOUT|EPOLLET;
+                ev.events=EPOLLIN|EPOLLET;
                 //修改sockfd上要处理的事件为EPOLLOUT
 
-                //epoll_ctl(epfd,EPOLL_CTL_MOD,sockfd,&ev);
+                epoll_ctl(epfd,EPOLL_CTL_MOD,sockfd,&ev);
 
-            }
-       
-        }
+           	 }
+      		} 
 }
    return 1;
-}
+}	
